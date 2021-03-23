0911f128f1f8301fc3efc387cbf0e15e570f404a
Edridge D'Souza <edridge@gmail.com>
2021-03-23 00:34:20 -0400

    Trim down data collection

--- Collect_Posts.py
+++ Collect_Posts.py
@@ -7,6 +7,7 @@ Created on Wed Nov 05 21:49:00 2014
 import praw
 import time
 import sqlite3
+from tqdm import tqdm
 
 
 class Scraper():
@@ -16,7 +17,7 @@ class Scraper():
         self.subreddit = self.r.get_subreddit(subreddit)
         self.failure_words = ['nazi', 'hitler', 'fascism', 'fascist']
 
-    def scrape(self):
+    def scrape(self, limit=5000):
         post_count = 0
 
         #get_new or get_top
@@ -25,62 +26,62 @@ class Scraper():
 
         conn = sqlite3.connect(self.db)
         cursor = conn.cursor()
-        while post_count < 5000 and not NoMorePosts:
+        while post_count < limit and not NoMorePosts:
             try:
-                for post in posts:
+                for post in tqdm(posts,
+                                 desc=f'Scraping from /r/{self.subreddit}'):
                     post_count += 1
                     self.process_post(post, conn, cursor)
             except Exception as e:
                 print(e)
                 NoMorePosts = True
+        conn.commit()
+        conn.close()
 
     def process_post(self, post, conn, cursor):
         time.sleep(1)
-        if post.num_comments > 100:
+        if post.num_comments > 100:  # Only allow posts above a certain size
             cursor.execute('''SELECT COUNT (*) 
-                                  FROM post 
-                                  WHERE post_id = ?''', (post.id, ))
-            if cursor.fetchone()[0] == 0 and post.num_comments > 1000:
-                post.replace_more_comments(limit=None, threshold=10)
-                flat_comments = praw.helpers.flatten_tree(
-                            post.comments)
+                            FROM post 
+                            WHERE post_id = ?''',
+                           (post.id, ))
+            if cursor.fetchone()[0] == 0:  # If post not yet in db
+                post.replace_more_comments(limit=None)
+                flat_comments = praw.helpers.flatten_tree(post.comments)
+
                 if self.text_fails(post.title + post.text):
-                    nazi_in_post = 1
+                    failure_in_post = 1
                 else:
-                    nazi_in_post = 0
+                    failure_in_post = 0
+
                 cursor.execute('''INSERT INTO post 
-                            (post_title, post_id, post_text, 
-                            nazi_in_post, subreddit, comment_num, 
-                            post_created)
-                            VALUES (?,?,?,?,?,?,?)''',
-                                       (post.title, post.id, post.selftext,
-                                        nazi_in_post, post.subreddit.display_name,
-                                        str(post.num_comments), post.created_utc))
+                                  (post_id, 
+                                  failure_in_post, 
+                                  subreddit, 
+                                  num_comments)
+                                  VALUES (?,?,?,?)''',
+                               (post.id, failure_in_post,
+                                post.subreddit.display_name,
+                                post.num_comments))
+
                 for comment in flat_comments:
                     if hasattr(comment, 'body'):
                         if self.text_fails(comment.body):
                             failure_in_comment = 1
                         else:
                             failure_in_comment = 0
+
                         cursor.execute('''
-                                INSERT INTO comment 
-                                (post_title, post_id, post_text, 
-                                    nazi_in_post, subreddit, comment_num, 
-                                    comment_url, comment_body, nazi_in_comment,
-                                    comment_created)
-                                    VALUES (?,?,?,?,?,?,?,?,?,?)''',
-                                               (post.title, post.id, post.selftext,
-                                                nazi_in_post, post.subreddit.display_name, str(
-                                                    post.num_comments),
-                                                comment.permalink,
-                                                comment.body, failure_in_comment,
-                                                comment.created_utc))
+                                       INSERT INTO comment 
+                                       (post_id, 
+                                       comment_url, 
+                                       failure_in_comment)
+                                       VALUES (?,?,?)''',
+                                       (post.id,
+                                        comment.permalink,
+                                        failure_in_comment))
                 conn.commit()
                 print("post added")
-            else:
-                print("already in db")
-        else:
-            print("skipping because fewer than 100")
 
     def text_fails(self, text):
         return any(item in text.lower() for item in self.failure_words)
--- Create Database.py
+++ Create Database.py
@@ -10,24 +10,20 @@ conn = sqlite3.connect("Godwin.db") # or use :memory: to put it in RAM
 cursor = conn.cursor()
 cursor.execute("DROP TABLE COMMENT")
 cursor.execute("DROP TABLE POST")
-cursor.execute("""CREATE TABLE post
-                  (post_title text, 
-                  post_id text, 
-                  post_text blob, 
-                  nazi_in_post integer,
-                  subreddit text, 
-                  comment_num integer, 
-                  post_created float)
-                  """)
-cursor.execute("""CREATE TABLE comment
-                  (post_title text, 
-                  post_id text, 
-                  post_text blob, 
-                  nazi_in_post integer,
-                  subreddit text,  
-                  comment_num integer, 
-                  comment_url text, 
-                  comment_body blob,
-                  nazi_in_comment integer,
-                  comment_created float)
-                  """)
+cursor.execute("""
+               CREATE TABLE post
+               (post_id text, 
+               failure_in_post integer,
+               subreddit text, 
+               num_comments integer);
+               """)
+cursor.execute("""
+               CREATE TABLE comment
+               (post_id text,
+               comment_url text, 
+               failure_in_comment integer,
+               
+               FOREIGN KEY (post_id)
+               REFERENCES post (post_id)
+               ON DELETE CASCADE);
+               """)
