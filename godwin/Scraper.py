60068ece9da15af428fd7d81d600c488c1ce91f7
Edridge D'Souza <edridge@gmail.com>
2021-03-23 19:18:51 -0400

    Use PushShift API to get large comment batches

--- godwin/Scraper.py
+++ godwin/Scraper.py
@@ -9,9 +9,10 @@ import json
 import os.path as path
 import sqlite3
 import sys
+import requests
 import time
 
-import praw  # TODO: switch to pushshift API psaw or pmaw
+import praw
 import requests
 from lxml import html
 from tqdm import tqdm
@@ -25,7 +26,8 @@ with open(path.abspath(cfg)) as f:
 
 
 class Scraper():
-    DELAY = 60/30 + 0.1  # Rate limit 30 requests per minute
+    PRAW_DELAY = 60/30 + 0.25  # Rate limit 30 requests per minute
+    PS_DELAY = 60/200 + 0.05  # Pushshift rate limit to 200 per minute
 
     def __init__(self, db: Database = Database('Godwin.db')):
         self.dbpath = db.path
@@ -41,17 +43,35 @@ class Scraper():
                               'eichmann', 'holocaust',
                               'auschwitz', 'swastika'}
 
-    def scrape(self, subreddit='all', limit=None):
+    def scrape_top_subreddits(self, limit=100):
+        page = requests.get('http://redditlist.com/')
+        tree = html.fromstring(page.text)
+
+        # creating list of subreddits
+        subs = tree.xpath('//*[@id="listing-parent"]/div[1]/div/span[3]/a')
+        subs = [s.text.lower() for s in subs if s.text != 'Home']
+        n_subs = len(subs)
+
+        # Start with smaller ones first
+        for sub_count, sub in enumerate(subs[::-1], 1):
+            self.scrape(subreddit=sub, limit=limit)
+            print(f'Scraped {sub_count} of {n_subs} subreddits',
+                  file=sys.stderr)
+
+        print('Done scraping')
+
+    def scrape(self, subreddit='all', time_filter='month', limit=None):
+        time.sleep(self.PRAW_DELAY)
         subreddit = self.r.subreddit(subreddit)
-        posts = subreddit.hot(limit=limit)
+        posts = subreddit.top(time_filter=time_filter, limit=limit)
 
         conn = sqlite3.connect(self.dbpath)
         cursor = conn.cursor()
-        
-        for post_count, post in tqdm(enumerate(posts), 
+
+        for post_count, post in tqdm(enumerate(posts),
                                      desc=f'Scraping from /r/{subreddit}'):
             self.process_post(post, cursor)
-            if post_count % 25 == 0:
+            if post_count and post_count % 25 == 0:
                 conn.commit()
 
         conn.commit()
@@ -72,8 +92,6 @@ class Scraper():
                            (post.id, ))
 
             if cursor.fetchone()[0] == 0:  # If post not yet in db
-                time.sleep(self.DELAY)
-
                 if self.text_fails(post.title + post.selftext):
                     failure_in_post = 1
                 else:
@@ -93,39 +111,50 @@ class Scraper():
                                 post.score,
                                 post.num_comments))
 
-                post.comments.replace_more(limit=None)
-                post.comment_sort = 'old'  # Ensure chronological order
-                flat_comments = post.comments.list()
+                values = self.process_comments(post.id)
 
-                for commentCount, comment in enumerate(flat_comments):
-                    if hasattr(comment, 'body'):
-                        if self.text_fails(comment.body):
-                            values = (post.id,
-                                      comment.id,
-                                      commentCount)
-                            cursor.execute('''
-                                       INSERT INTO failures 
-                                       (post_id, 
-                                       comment_id,
-                                       num_prev_comments)
-                                       VALUES (?,?,?)''',
-                                           values)
-                            return values
-        return None
+                if values[1] is not None:
+                    cursor.execute('''
+                                    INSERT INTO failures 
+                                    (post_id, 
+                                    comment_id,
+                                    num_prev_comments)
+                                    VALUES (?,?,?)''',
+                                   values)
 
-    def scrape_top_subreddits(self, limit=100):
-        page = requests.get('http://redditlist.com/')
-        tree = html.fromstring(page.text)
+    def process_comments(self, postid):
+        session = requests.Session()  # https://stackoverflow.com/a/45470227/15014819
+        session.hooks = {
+            'response': lambda r, *args, **kwargs: r.raise_for_status()
+        }
 
-        #creating list of subreddits
-        subs = tree.xpath('//*[@id="listing-parent"]/div[1]/div/span[3]/a')
-        subs = [s.text.lower() for s in subs if s.text != 'Home']
+        block_size = 20000
+        comment_index = 0
+        after = 0
 
-        for sub_count, sub in enumerate(subs[::-1], 1):  # Start with smaller ones first
-            self.scrape(subreddit=sub, limit=limit)
-            print(f'Scraped {sub_count} subreddits', file=sys.stderr)
+        while block_size == 20000:
+            try:
+                params = {'link_id': postid, 'limit': 20000,  # `limit` lets you use 20k, `size` only gives 100
+                          'sort': 'asc', 'after': after}
+                comments = session.get('https://api.pushshift.io/reddit/comment/search',
+                                       params=params)
+                results = comments.json()['data']
+                block_size = len(results)
 
-        print('Done scraping')
+                if results:
+                    for comment in results:
+                        comment_index += 1
+                        if self.text_fails(comment['body']):
+                            return (postid, comment['id'], comment_index)
+
+                    after = results[-1]['created_utc']
+                
+                # time.sleep(self.PS_DELAY)  # Honestly probably unnecessary
+
+            except requests.HTTPError:
+                print(f'Error with comments from post {postid}')
+
+        return (None, None, None)
 
     def text_fails(self, text):
         return any(item in text.lower() for item in self.failure_words)
